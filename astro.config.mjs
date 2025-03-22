aa7a28d22b84c7552b51deec072d0e34332da11a
Christine Tham <chris.tham@hellotham.com>
2025-03-22 11:31:37 +1100

    Update changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,25 @@
-import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-import alpinejs from '@astrojs/alpinejs';
-import robotsTxt from 'astro-robots-txt';
-import remarkEmoji from 'remark-emoji';
-import { remarkReadingTime } from './remark-plugins/remark-reading-time.mjs';
-import UnoCSS from 'unocss/astro';
-
+import { defineConfig } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
+import alpinejs from '@astrojs/alpinejs'
+import robotsTxt from 'astro-robots-txt'
+import remarkEmoji from 'remark-emoji'
+import { remarkReadingTime } from './remark-plugins/remark-reading-time.mjs'
+import UnoCSS from 'unocss/astro'
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    ssr: {
-      external: ['svgo'],
-      noExternal: ['swiper']
-    }
-  },
   site: 'https://airsydneyhills.netlify.app',
-  integrations: [UnoCSS({
-    injectReset: true // or a path to the reset file
-  }), sitemap(), alpinejs(), robotsTxt()],
+  integrations: [
+    UnoCSS({
+      injectReset: true // or a path to the reset file
+    }),
+    sitemap(),
+    alpinejs(),
+    robotsTxt()
+  ],
   markdown: {
     extendDefaultPlugins: true,
-    remarkPlugins: [remarkReadingTime, remarkEmoji],
-    shikiConfig: {
-      theme: 'github-light',
-      langs: [],
-      // Enable word wrap to prevent horizontal scrolling
-      wrap: true
-    },
+    remarkPlugins: [remarkReadingTime, remarkEmoji]
   },
-  scopedStyleStrategy: "where",
-});
+  scopedStyleStrategy: 'where'
+})
